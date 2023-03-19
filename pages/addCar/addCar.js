532289d73f77b1b0a5b8b4c5160dfc323c645e77
ferh0162 <ferh0162@stud.kea.dk>
2023-03-19 11:43:06 +0100

    secuirty

--- pages/addCar/addCar.js
+++ pages/addCar/addCar.js
@@ -1,9 +1,33 @@
 
 import { API_URL,FETCH_NO_API_ERROR } from "../../settings.js"
+import {handleHttpErrors} from "../../utils.js"
 //Add id to this URL to get a single user
 const URL = `${API_URL}/cars`
 
 export async function initAddCar(match) {
-  
+  document.getElementById("btn-submit-car").onclick = makeCar
+}
+
+async function makeCar(){
+    const brand = document.getElementById("brand").value
+    const model = document.getElementById("model").value
+    const pricePrDay = document.getElementById("price-pr-day").value
+    const bestDiscount = document.getElementById("best-discount").value
+
+    const body = {brand, model, pricePrDay, bestDiscount}
+
+    const options = {
+    "method" : "POST",
+    "headers": {"Content-type": "application/json",
+    "Authorization" : "Bearer " + localStorage.getItem("token")
+    },
+    "body" : JSON.stringify(body)
+    }
+try {
+    const newCar = await fetch(URL, options).then(res=>handleHttpErrors(res))
+    document.getElementById("status").innerText = JSON.stringify(newCar)
+} catch (err) {
+    alert(err.message)
+}
 }
 
--- pages/cars/cars.js
+++ pages/cars/cars.js
@@ -1,6 +1,37 @@
 import { API_URL } from "../../settings.js"
-const URL = API_URL + "/cars/admin"
+import { handleHttpErrors, sanitizeStringWithTableRows } from "../../utils.js"
+const URL = API_URL + "/cars"
 
 export async function initCars() {
+    getAllCars()
+}
+
+async function getAllCars(){
+    try {
+
+        const options = {
+            method: "GET",
+            Headers: {"Authorization": "Bearer " + localStorage.getItem("token")}
+        }
+        
+        const cars = await fetch(URL,options).then(res=>handleHttpErrors(res))
+        
+        const tableRows = cars.map(car => `
+        <tr>
+        <td>${car.id}</td>
+        <td>${car.brand}</td>
+        <td>${car.model}</td>
+        <td>${car.pricePrDay}</td>
+        <td>${car.bestDiscount}</td>
+        </tr>
+        `).join("")
+
+        document.getElementById("table-rows").innerHTML =
+        sanitizeStringWithTableRows(tableRows)
+
+    } catch (ex) {
+        document.getElementById("error").innerText = ex.message
+
+    }
 }
 
